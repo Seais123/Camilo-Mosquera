456fd757558d42b4547a4a067a6b1781041774ab
Seais123 <130000560+Seais123@users.noreply.github.com>
2023-05-13 21:06:42 -0500

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,3 @@
+var derechosjs = document.getElementById(derechosjs);
+derechosjs.style.left = "100px";/* muve el objeto a 100 pixeles a la derecha */
+derechosjs.style.top = "200px"; /* mueve el objeto a 200 pixeles hacia abajo*/
